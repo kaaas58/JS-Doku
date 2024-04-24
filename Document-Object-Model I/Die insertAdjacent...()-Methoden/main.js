66c15d567165e84ad86797eeecb94c37c56608a2
kaaas58 <lutr58@googlemail.com>
2024-04-24 19:19:54 +0200

    lilfix

--- Document-Object-Model I/Die insertAdjacent...()-Methoden/main.js
+++ Document-Object-Model I/Die insertAdjacent...()-Methoden/main.js
@@ -2,3 +2,15 @@
 
 let li = document.createElement("li");
 li.setAttribute("id", "mein-listenelement");
+console.log(li);
+
+let a = document.createElement("a")
+a.setAttribute("id", "mein-ankerelement");
+a.setAttribute("href", "#");
+
+let text = document.createTextNode("Element");
+
+a.appendChild(text);
+li.appendChild(a);
+
+console.log(li);
