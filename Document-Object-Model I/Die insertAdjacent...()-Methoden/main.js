cc2589eb96ae3fe3eb531b093cedcfa9aecfcf0c
kaaas58 <lutr58@googlemail.com>
2024-04-24 22:38:26 +0200

    next lines

--- Document-Object-Model I/Die insertAdjacent...()-Methoden/main.js
+++ Document-Object-Model I/Die insertAdjacent...()-Methoden/main.js
@@ -25,22 +25,36 @@ li.appendChild(a);
 // Alles innerhalb der ul in #navigation holen und in ket liste halten.
 let liste = document.querySelector("#navigation > ul");
 // Der liste/"#navigation > ul" => mit appendChild unser li anhängen, mitgeben (auf letztem Platz in ul.)
-// liste.appendChild(li);
-
 
 // In (let liste) => (insert) + wo(Adjacend=benachbartes) + was(Element).
+
+// ---------------------------------------------------- insertAdjacent
+
+// ------------------------------------------------- insertAdjacentElement
 
 // liste.insertAdjacentElement("beforebegin", li);
 // liste.insertAdjacentElement("afterbegin", li);
 // liste.insertAdjacentElement("beforeend", li);
 // liste.insertAdjacentElement("afterend", li);
 
-// In (let liste) => (insert) + wo(Adjanced=benachbartes) + was(HTML).
-// DOM-String
+// --------------------------------------------------- insertAdjacentHTML
+
+// DOM-String => Kombination der oben einzelnen Elemente.
 // Bei li mit Backslash die 2 ten Anführungszeichen escapen  \".........\" .
 let dom_string = "<li id=\"mein-listelement\"><a id=\"mein-ankerelement\" href=\"#\">Element</a></li>";
 
+// In (let liste) => (insert) + wo(Adjanced=benachbartes) + was(HTML).
+// let liste wurde oben referenziert.
 // liste.insertAdjacentHTML("beforebegin", dom_string);
 // liste.insertAdjacentHTML("afterbegin", dom_string);
 // liste.insertAdjacentHTML("beforeend", dom_string);
 // liste.insertAdjacentHTML("afterend", dom_string);
+
+// -------------------------------------------------- insertAdjacentText
+
+let text2 = "Loremipsum dolor sit amet";
+
+// liste.insertAdjacentText("beforebegin", text2);
+// liste.insertAdjacentText("afterbegin", text2);
+// liste.insertAdjacentText("beforeend", text2);
+// liste.insertAdjacentText("afterend", text2);
