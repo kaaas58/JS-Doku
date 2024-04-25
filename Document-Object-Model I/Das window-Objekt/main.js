e0460748d3e0305b68d92e356fb1bff1ebbdebbc
kaaas58 <lutr58@googlemail.com>
2024-04-25 20:09:29 +0200

    Automatisches

--- Document-Object-Model I/Das window-Objekt/main.js
+++ Document-Object-Model I/Das window-Objekt/main.js
@@ -1,4 +1,9 @@
 "use strict";
 
-
+console.log(window);
+// Eigenschaften des Window-Objekts
+// Man kann bei dem Window-Objekt das window weg lassen
+// Also window.innerWidth => innerWidth
+console.log(innerWidth);
+console.log(innerHeight);
 
