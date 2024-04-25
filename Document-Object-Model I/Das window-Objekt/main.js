5ea35e5794a10f2a5bb1a8a98c4a35eb8de637d3
kaaas58 <lutr58@googlemail.com>
2024-04-25 21:22:31 +0200

    Automatisches

--- Document-Object-Model I/Das window-Objekt/main.js
+++ Document-Object-Model I/Das window-Objekt/main.js
@@ -4,6 +4,21 @@ console.log(window);
 // Eigenschaften des Window-Objekts
 // Man kann bei dem Window-Objekt das window weg lassen
 // Also window.innerWidth => innerWidth
+
+// Sichtbarer Bereich incl ScrollBar, nach resizen z.B. 
+    // mit Maus ändert sich das logischer Weiße.
 console.log(innerWidth);
 console.log(innerHeight);
+
+// // // // // // // // // // Outer inner vertauscht...FIXME // // // // // // // // //
+
+// Bereich ganzes Browser Fenster
+console.log("---");
+console.log(outerWidth);
+console.log(outerHeight);
+
+// Gibt die Scrollposition des Fensters an, links oben => X, Y 0, 0.
+    // Bewegungen Nutzer oder Fenster können getrackt werden.
+console.log(scrollX);   // X Links = 0 / Rechts = (+) Bsp(121..) horizontal .
+console.log(scrollY);   // Y Oben = 0 / Unten = (+) Bsp(123..) vertical .
 
