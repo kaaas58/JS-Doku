5cb1e50cbb6c44fdc45971b5c1a7f03ca13e190a
kaaas58 <lutr58@googlemail.com>
2024-04-27 17:03:08 +0200

    clean

--- A_liqui-planner/src/js/main.js
+++ A_liqui-planner/src/js/main.js
@@ -18,9 +18,9 @@ const haushaltsbuch = {
             this.eintraege.push(neuer_eintrag);
         }else{
             console.log("Folgende Fehler wurden gefunden !");
-            this.fehler.forEach(function(fehler){
-                console.log(fehler);
-            });
+            this.fehler.forEach(fehler => 
+                console.log(fehler)
+            );
         }
     },
 // ------------------------------------ typ_verarbeiten mit typ_validieren
@@ -91,7 +91,7 @@ const haushaltsbuch = {
     },
 // -------------------------------------------- eintraege_sortieren 
     eintraege_sortieren() {
-        this.eintraege.sort(function(eintrag_a, eintrag_b) {
+        this.eintraege.sort((eintrag_a, eintrag_b) =>{ 
             if (eintrag_a.get("datum") > eintrag_b.get("datum")) {
                 return -1;
             } else if (eintrag_a.get("datum") < eintrag_b.get("datum")) {
@@ -99,7 +99,7 @@ const haushaltsbuch = {
             } else {
                 return 0;
             }
-        });
+    });
     },
 
     html_eintrag_generieren(eintrag){                                                              
@@ -166,14 +166,11 @@ const haushaltsbuch = {
 
     eintraege_anzeigen(){
 
-            document.querySelectorAll(".monatsliste ul").forEach(function(eintragsliste){
-                eintragsliste.remove();
-            });
+            document.querySelectorAll(".monatsliste ul").forEach(eintragsliste => eintragsliste.remove());
 
             let eintragsliste = document.createElement("ul");
-            for(let eintrag of this.eintraege){
-                eintragsliste.insertAdjacentElement("beforeend", this.html_eintrag_generieren(eintrag));
-            }
+            this.eintraege.forEach(eintrag => eintragsliste.insertAdjacentElement("beforeend", this.html_eintrag_generieren(eintrag))
+            );
             document.querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
     },
 
@@ -184,7 +181,7 @@ const haushaltsbuch = {
         neue_gesamtbilanz.set("einnahmen", 0);
         neue_gesamtbilanz.set("ausgaben", 0);
         neue_gesamtbilanz.set("bilanz", 0);
-        this.eintraege.forEach(function(eintrag) {
+        this.eintraege.forEach((eintrag) => {
             switch (eintrag.get("typ")) {
                 case "einnahme":
                 // case "Einnahme":
@@ -203,16 +200,6 @@ const haushaltsbuch = {
         });
         this.gesamtbilanz = neue_gesamtbilanz;
     },
-  
-
-    
-    // <!-- Gesamtbilanz -->
-    // <aside id="gesamtbilanz">
-    //     <h1>Gesamtbilanz</h1>
-    //     <div class="gesamtbilanz-zeile einnahmen"><span>Einnahmen:</span><span>0,00€</span></div>
-    //     <div class="gesamtbilanz-zeile ausgaben"><span>Ausgaben:</span><span>0,00€</span></div>
-    //     <div class="gesamtbilanz-zeile bilanz"><span>Bilanz:</span><span class="positiv">0,00€</span></div>
-    // </aside>
 
     html_gesamtbilanz_generieren(){
         
@@ -264,20 +251,12 @@ const haushaltsbuch = {
 
     gesamtbilanz_anzeigen(){
 
-        document.querySelectorAll("#gesamtbilanz").forEach(function(gesamtbilanz){
-                gesamtbilanz.remove();
-        });
+        document.querySelectorAll("#gesamtbilanz").forEach((gesamtbilanz) => 
+                gesamtbilanz.remove()
+        );
         document.querySelector("body").insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
     },
 
-    // ---------------------------------- gesamtbilanz ausgeben()
-                // prüfen ob bereits Gesammtbilanz angezeigt wird wenn ja entfernen
-                    // neue Gesmtbilanz anzeigen ges generieren
-
-
-
-// -------------------------------------------- eintrag_hinzufuegen
-//                                                steuert alles
     eintrag_hinzufuegen() {
         let weiterer_eintrag = true;
         while (weiterer_eintrag) {
