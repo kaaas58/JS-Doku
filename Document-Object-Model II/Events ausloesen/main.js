04410ae0b7ca9d6bc784f3caf109c6c202a6cd92
kaaas58 <lutr58@googlemail.com>
2024-04-28 00:20:43 +0200

    Automatisches

--- Document-Object-Model II/Events ausloesen/main.js
+++ Document-Object-Model II/Events ausloesen/main.js
@@ -2,4 +2,10 @@
 
 let link = document.querySelector("a.active");
 let input = document.querySelector("input[type=text]");
-let formular = document.querySelector("from");
+let formular = document.querySelector("form");
+
+setTimeout(() => link.click(), 3000);
+setTimeout(() => input.focus(), 6000);
+setTimeout(() => input.blur(), 9000);
+setTimeout(() => formular.reset(), 12000);
+setTimeout(() => formular.submit(), 15000);
