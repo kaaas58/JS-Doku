c8578e6149d301b9644b98dd1ec8cc0497b5d282
kaaas58 <lutr58@googlemail.com>
2024-05-15 00:02:55 +0200

    weiteaufbau an eingabeformular zum abfangen fon Fehlern

--- A_liqui-planner/src/js/eingabeformular.js
+++ A_liqui-planner/src/js/eingabeformular.js
@@ -3,31 +3,74 @@
 const eingabeformular = {
 
     formulardaten_holen(e){
-        let typ;
-        if(e.target.elements.ausgabe.checked === true) {
-            typ = "ausgabe";
-        } else if(e.target.elements.einnahme.checked === true){
-            typ = "einnahme";
-        }
-        
+       
         return {
             titel: e.target.elements.titel.value,
             betrag: e.target.elements.betrag.value,
-            typ: typ,
+            einnahme: e.target.elements.einnahme.checked,
+            ausgabe: e.target.elements.ausgabe.checked,
             datum: e.target.elements.datum.valueAsDate
         }
     },
 
+    formulardaten_verarbeiten(formulardaten){
+
+        let typ;
+        if(formulardaten.einnahme === true) {
+            typ = "einnahme";
+        } else if(formulardaten.ausgabe === true){
+            typ = "ausgabe";
+        }
+
+        return {
+            titel: formulardaten.titel.trim(),
+            typ: typ,
+            betrag: parseFloat(formulardaten.betrag) * 100,
+            datum: formulardaten.datum
+        }
+    },
+
+    formulardaten_validieren(formulardaten){
+
+        let fehler = [];
+        if(formulardaten.titel === ""){
+            fehler.push("Titel");
+        }
+        if (formulardaten.typ === undefined || formulardaten.typ.match(/^(?:einnahme|ausgabe)$/) === null){
+            fehler.push("Typ");
+        }
+        if (isNaN(formulardaten.betrag)){
+            fehler.push("Betrag");
+        }
+        if (formulardaten.datum === null){
+            fehler.push("Datum");
+        }
+        return fehler;
+    },
+
+    datum_aktualisieren(){
+        let datum_imput = document.querySelector("#datum");
+        if(datum_imput !== null){
+            datum_imput.valueAsDate = new Date();
+        }
+    },
 
     absenden_event_hinzufuegen(eingabeformular) {
         eingabeformular.querySelector("#eingabeformular").addEventListener("submit", e => {
             e.preventDefault();
-            // formulartdaten hole
-            console.log(e);
-            let formulardaten = this.formulardaten_holen(e);
+            // formulartdaten hole und Formulardaten verarbeiten
+            let formulardaten = this.formulardaten_verarbeiten(this.formulardaten_holen(e));
             console.log(formulardaten);
-            // formulardaten verarbeiten
-            // formulardaten validieren
+            let formular_fehler = this.formulardaten_validieren(formulardaten);
+            console.log(formular_fehler);
+
+            if(formular_fehler.length === 0){
+                haushaltsbuch.eintrag_hinzufuegen(formulardaten)
+                e.target.reset();
+                this.datum_aktualisieren();
+            }else{
+
+            }
             // wenn formulardaten falide sind
                 // Eintrag zum Haushaltsbuch hinzufuegen
                 // wenn bereits Fehlermeldung angezeigtr wird 
@@ -45,30 +88,38 @@ const eingabeformular = {
             
             let eingabeformular = document.createElement("section");
             eingabeformular.setAttribute("id", "eingabeformular-container");
-            eingabeformular.innerHTML = `<form id="eingabeformular" action="#" method="get"></form>
-            <div class="eingabeformular-zeile">
-            <h1>Neue Einnahme / Ausgabe hinzufügen</h1>
+            eingabeformular.innerHTML = `<div class="fehlerbox">
+                <span>Es gibt Fehler in folgenden Eingabefeldern:</span>
+                <ul>
+                    <li>Titel</li>
+                    <li>Betrag</li>
+                    <li>Datum</li>
+                </ul>
             </div>
+            <form id="eingabeformular" action="#" method="get"></form>
             <div class="eingabeformular-zeile">
-            <div class="titel-typ-eingabe-gruppe">
-            <label for="titel">Titel</label>
-            <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size="10" title="Titel des Eintrags">
-            <input type="radio" id="einnahme" name="typ" value="einnahme" form="eingabeformular" title="Typ des Eintrags">
-            <label for="einnahme" title="Typ des Eintrags">Einnahme</label>
-            <input type="radio" id="ausgabe" name="typ" value="ausgabe" form="eingabeformular" title="Typ des Eintrags" checked>
-            <label for="ausgabe" title="Typ des Eintrags">Ausgabe</label>
-            </div>
+                <h1>Neue Einnahme / Ausgabe hinzufügen</h1>
             </div>
             <div class="eingabeformular-zeile">
-            <div class="betrag-datum-eingabe-gruppe">
-            <label for="betrag">Betrag</label>
-            <input type="number" id="betrag" name="betrag" form="eingabeformular" placeholder="z.B. 10,42" size="10" step="0.01" title="Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)">
-            <label for="datum">Datum</label>
-            <input type="date" id="datum" name="datum" form="eingabeformular" placeholder="jjjj-mm-tt" size="10" title="Datum des Eintrags (Format: jjjj-mm-tt)">
-            </div>
+                <div class="titel-typ-eingabe-gruppe">
+                    <label for="titel">Titel</label>
+                    <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size="10" title="Titel des Eintrags">
+                    <input type="radio" id="einnahme" name="typ" value="einnahme" form="eingabeformular" title="Typ des Eintrags">
+                    <label for="einnahme" title="Typ des Eintrags">Einnahme</label>
+                    <input type="radio" id="ausgabe" name="typ" value="ausgabe" form="eingabeformular" title="Typ des Eintrags" checked> 
+                    <label for="ausgabe" title="Typ des Eintrags">Ausgabe</label>
+                </div>
             </div>
             <div class="eingabeformular-zeile">
-            <button class="standard" type="submit" form="eingabeformular">Hinzufügen</button>
+                <div class="betrag-datum-eingabe-gruppe">
+                    <label for="betrag">Betrag</label>
+                    <input type="number" id="betrag" name="betrag" form="eingabeformular" placeholder="z.B. 10,42" size="10" step="0.01" title="Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)">
+                    <label for="datum">Datum</label>
+                    <input type="date" id="datum" name="datum" form="eingabeformular" placeholder="jjjj-mm-tt" size="10" title="Datum des Eintrags (Format: jjjj-mm-tt)">
+                </div>
+            </div>
+            <div class="eingabeformular-zeile">
+                <button class="standard" type="submit" form="eingabeformular">Hinzufügen</button>
             </div>`;
             
             this.absenden_event_hinzufuegen(eingabeformular);
@@ -78,7 +129,7 @@ const eingabeformular = {
         
         anzeigen() {
             document.querySelector("#navigationsleiste").insertAdjacentElement("afterend", this.html_generieren());
-            // Datum auf den heutigen tag setzen
+            this.datum_aktualisieren();
         }
         
     };
--- A_liqui-planner/src/js/haushaltsbuch.js
+++ A_liqui-planner/src/js/haushaltsbuch.js
@@ -4,91 +4,21 @@ const haushaltsbuch = {
 
     gesamtbilanz: new Map(),
     eintraege: [],
-    fehler: [],
 
-// -------------------------------------------- eintrag_erfassen
-    eintrag_erfassen() {
+    eintrag_hinzufuegen(formulardaten) {
         let neuer_eintrag = new Map();
-        neuer_eintrag.set("titel", this.titel_verarbeiten(prompt("Titel:")));
-        neuer_eintrag.set("typ", this.typ_verarbeiten(prompt("Typ (Einnahme oder Ausgabe):")));
-        neuer_eintrag.set("betrag", this.betrag_verarbeiten(prompt("Betrag (in Euro, ohne €-Zeichen):")));
-        neuer_eintrag.set("datum", this.datum_verarbeiten(prompt("Datum (jjjj-mm-tt):")));
+        neuer_eintrag.set("titel", formulardaten.titel);
+        neuer_eintrag.set("betrag", formulardaten.betrag);
+        neuer_eintrag.set("typ", formulardaten.typ);
+        neuer_eintrag.set("datum", formulardaten.datum);
         neuer_eintrag.set("timestamp", Date.now());
-        if(this.fehler.length === 0){
-            this.eintraege.push(neuer_eintrag);
-        }else{
-            console.log("Folgende Fehler wurden gefunden !");
-            this.fehler.forEach(fehler => 
-                console.log(fehler)
-            );
-        }
-    },
-// ------------------------------------ typ_verarbeiten mit typ_validieren
-    typ_verarbeiten(typ) {
-        typ = typ.trim().toLowerCase();
-        if (this.typ_validieren(typ)) {
-            return typ;
-        } else {
-            this.fehler.push(`Ungültiger Eintrags-Typ: "${typ}".`);
-        }
-    },
-    typ_validieren(typ) {
-        if (typ.match(/^(?:einnahme|ausgabe)$/) !== null) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-// --------------------------------- titel_verarbeiten mit titel_validieren
-    titel_verarbeiten(titel) {
-        titel = titel.trim();
-        if (this.titel_validieren(titel)) {
-            return titel;
-        } else {
-            this.fehler.push("Kein Titel angegeben.");
-        }
-    },
-    titel_validieren(titel) {
-        if (titel !== "") {
-            return true;
-        } else {
-            return false;
-      
-      }
-    },
+        this.eintraege.push(neuer_eintrag);
+        this.eintraege_sortieren();
+        this.eintraege_anzeigen();
+        this.gesamtbilanz_erstellen();
+        this.gesamtbilanz_anzeigen();
+},  
 
-// -------------------------------  betrag_verarbeiten mit betrag-validieren
-    betrag_verarbeiten(betrag) {
-        betrag = betrag.trim();
-        if (this.betrag_validieren(betrag)) {
-            return parseFloat(betrag.replace(",", ".")) * 100;
-        } else {
-            this.fehler.push(`Ungültiger Betrag: ${betrag} €.`);
-        }
-    },
-    betrag_validieren(betrag) {
-        if (betrag.match(/^\d+(?:(?:,|\.)\d\d?)?$/) !== null) {
-            return true;
-        } else {
-            return false;
-        }
-    },
- // ---------------------------------- datum_verarbeiten mit datum-validieren
-    datum_verarbeiten(datum) {
-        datum = datum.trim();
-        if (this.datum_validieren(datum)) {
-            return new Date(`${datum} 00:00:00`);
-        } else {
-            this.fehler.push(`Ungültiges Datumsvormat: "${datum}".`);
-        }
-    },
-    datum_validieren( datum) {
-        if (datum.match(/^\d{4}-\d{2}-\d{2}$/) !== null) {
-            return true;
-        } else {
-            return false;
-        }
-    },
 // -------------------------------------------- eintraege_sortieren 
     eintraege_sortieren() {
         this.eintraege.sort((eintrag_a, eintrag_b) =>{ 
@@ -255,22 +185,5 @@ const haushaltsbuch = {
                 gesamtbilanz.remove()
         );
         document.querySelector("body").insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
-    },
-
-    eintrag_hinzufuegen() {
-        let weiterer_eintrag = true;
-        while (weiterer_eintrag) {
-            this.eintrag_erfassen();
-            if (this.fehler.length === 0){
-                this.eintraege_sortieren();
-                this.eintraege_anzeigen();
-                this.gesamtbilanz_erstellen();
-                this.gesamtbilanz_anzeigen();
-            }else{
-                this.fehler = []; // Setzt array auf leer.
-            }
-            weiterer_eintrag = confirm("Weiteren Eintrag hinzufügen ?");
-        }
     }
-
 };
