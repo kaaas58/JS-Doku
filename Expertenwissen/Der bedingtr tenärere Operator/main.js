a94e272ba0242eeb18c705f1d1f06ce9e542fc37
kaaas58 <lutr58@googlemail.com>
2024-05-15 19:22:45 +0200

    Automatisches

---
+++ Expertenwissen/Der bedingtr tenärere Operator/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Synta: Bedingung ? Ausdruck_1 : Ausdruck_2
+
+// if (Bedingung){
+//     Ausdruck_1;
+// }else{
+//     Ausdruck_2;
+// }
+
+let meine_zahl = 10;
+
+// if(meine_zahl > 10){
+//     console.log("Größer als 10!");
+// }else{
+//     console.log("Kleiner als");
+// }
+
+console.log(meine_zahl <=9 ? "10 oder größer" : "Kleiner als 10");
+
+let geschlecht = 0;
+console.log(`Hallo, ${geschlecht ? "Frau" : "Herr"} Mustermann !`);
