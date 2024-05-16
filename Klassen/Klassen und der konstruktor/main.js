36fd6c4cf2d70639c8465fb7fd6ab36aa12228bf
kaaas58 <lutr58@googlemail.com>
2024-05-16 19:33:53 +0200

    first classes

---
+++ Klassen/Klassen und der konstruktor/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// let konto = {
+//     iban: "DE6206752564419854",
+//     inhaber: ["Max Mustermann"],
+//     saldo: 3500,
+//     aktiv: true,
+//     einzahlen(einzahlung) {
+//         this.saldo += einzahlung;
+//     },
+//     abheben(auszahlung) {
+//         this.saldo -= auszahlung;
+//     },
+//     kontostand_abfragen() {
+//         return this.saldo;
+//     }
+// };
+
+// console.log(konto);
+
+
+let k = new Konto("DE6206752564419854", "Max Mustermann", 3500);
+console.log(k);
