010f9651973eaad1f9716e5bb9d3215ce206c70a
kaaas58 <lutr58@googlemail.com>
2024-05-17 18:57:46 +0200

    class navigationsleiste implemented

--- A_liqui-planner/src/js/classes/Navigationsleiste.js
+++ A_liqui-planner/src/js/classes/Navigationsleiste.js
@@ -7,5 +7,35 @@
 
 class Navigationsleiste{
 
+    constructor(){
+        this._html = this._html_generieren();
+    }
+
+    _html_generieren(){
+        let navigationsleiste = document.createElement("nav");
+        navigationsleiste.setAttribute("id", "navigationsleiste");
+      navigationsleiste
+
+
+        let anker = document.createElement("a");
+        anker.setAttribute("href", "#" );                       // # ist index.html bei onepage
+
+        let span = document.createElement("span");
+        span.setAttribute("id", "markenname");
+        span.textContent = "Liqui_Planer";
+        anker.insertAdjacentElement("afterbegin", span);
+
+        navigationsleiste.insertAdjacentElement("afterbegin", anker);
+
+        return navigationsleiste;
+    }
+
+    anzeigen(){
+        let body = document.querySelector("body");
+        if (body !== null) {                                    //alternativ, bedingter tenaerewr ausdruck 
+            body.insertAdjacentElement("afterbegin", this._html);    //  body !== null ? body.insertAdjacentElement("afterbegin", this._html) : null;
+        }
+    }
+    
     
 }
